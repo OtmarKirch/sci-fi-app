eb8fde1bbc9e0920d3569a622062e79dc6cf0ffa
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-04-30 09:35:37 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const { timeStamp } = require("console");
+
+http
+  .createServer((request, response) => {
+    const addr = request.url;
+    const q = new URL(addr, "http://localhost:8080");
+    let filePath = "";
+
+    fs.appendFile(
+      "log.txt",
+      "URL: " + addr + "\n Timestamp: " + new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Entry logged.");
+        }
+      }
+    );
+
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+      console.log("documentation");
+    } else {
+      filePath = "index.html";
+    }
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write(data);
+      response.end();
+    });
+  })
+  .listen(8080);
+
+console.log("Local server is running on Port 8080.");
