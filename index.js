b9639346c198fae3d79882bb96fd7b2b8f0bf050
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-05-22 11:09:47 +0200

    Implemented free access for movies

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ app.get("/", (req, res) => {
 //return all movies vom the db as json file
 app.get(
   "/movies",
-  passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Movies.find()
       .then((movies) => {
